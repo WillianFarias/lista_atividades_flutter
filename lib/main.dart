cbd7b9209851920aa1452761be09e38b63225020
WillianFarias <williansousafarias@gmail.com>
2022-05-29 12:28:09 -0300

    ajustes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:lista_atividade/view/lista_atividade.dart';
 
-void main() {}
+void main() {
+  runApp((MaterialApp(
+    theme: ThemeData(
+        primaryColor: Colors.amber[900], //accentColor: Colors.blueAccent[700],
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+          secondary: Colors.blueAccent,
+        ),
+        buttonTheme: ButtonThemeData(
+            buttonColor: Colors.blueAccent[700],
+            textTheme: ButtonTextTheme.primary)),
+    home: Home(),
+  )));
+}
